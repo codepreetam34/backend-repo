2625197efea17bdc8c90e1e3ab4adc7a26912db3
unknown <preetamkumar3232@gmail.com>
2023-03-13 10:37:54 +0530

    resolved address

--- common-middleware/index.js
+++ common-middleware/index.js
@@ -43,8 +43,6 @@ exports.requireSignin = (req, res, next) => {
     const token = req.headers.authorization.split(" ")[1];
     const user = jwt.verify(token, process.env.JWT_SECRET);
     req.user = user;
-    console.log("user", user);
-    console.log("user Data", user.user);
   } else {
     return res
       .status(400)
