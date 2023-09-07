f7db3533e4754c685747744b43207289d031d265
Preetam Kumar <preetamkumar3232@gmail.com>
2023-09-07 19:01:29 +0530

    added homepage banner flow

--- controllers/homepageBanner.js
+++ controllers/homepageBanner.js
@@ -6,7 +6,6 @@ const homepageBanner = require("../models/homepageBanner");
 exports.createBanner = (req, res) => {
   const { title, type, createdBy } = req.body;
   let banners = [];
-  
   if (req.files.length > 0) {
     banners = req.files.map((file) => {
       return { img: process.env.API + "/public/" + file.filename };
@@ -72,11 +71,28 @@ exports.deleteBannerById = (req, res) => {
 };
 
 exports.getBanners = async (req, res) => {
-  homepageBanner.find({}).exec((error, banners) => {
-    if (error) return res.status(400).json({ error });
-    if (banners) {
-      res.status(200).json({ banners });
-    }
-  });
+  const limit = parseInt(req.query.limit) || 10; // Set a default of 10 items per page
+  const page = parseInt(req.query.page) || 1; // Set a default page number of 1
 
+  try {
+    const banners = await homepageBanner
+      .find({})
+      .sort({ _id: -1 })
+      .limit(limit)
+      .skip(limit * page - limit);
+
+    const count = await homepageBanner.countDocuments().exec();
+    const totalPages = Math.ceil(count / limit);
+
+    if (banners) {
+      res.status(200).json({
+        homePageBanners: banners,
+        pagination: { currentPage: page, totalPages, totalItems: count },
+      });
+    } else {
+      return res.status(400).json({ error: error.message });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
