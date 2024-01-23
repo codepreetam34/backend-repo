00373fa2c071f8eb7551962ee96edb351ab09d8a
codepreetam34 <preetamkumar3232@mail.com>
2024-01-23 17:54:36 +0530

    added address component and profile comp

--- routes/auth.js
+++ routes/auth.js
@@ -1,39 +1,60 @@
 const express = require("express");
-const { signup,signin,signout,updatePassword, updateProfile, forgotPassword,requestVerifyEmail,verifyEmailViaOtp, resetPassword, requestPasswordReset, generateOTP, verifyOTP, getUserData } = require("../controllers/auth");
-const { validateSignupRequest, isRequestValidated, validateSigninRequest } = require('../validators/auth');
-const { requireSignin, localVariable, upload } = require('../common-middleware');
+const {
+  signup,
+  signin,
+  signout,
+  updatePassword,
+  updateProfile,
+  forgotPassword,
+  requestVerifyEmail,
+  verifyEmailViaOtp,
+  resetPassword,
+  requestPasswordReset,
+  generateOTP,
+  verifyOTP,
+  getUserData,
+} = require("../controllers/auth");
+const {
+  validateSignupRequest,
+  isRequestValidated,
+  validateSigninRequest,
+} = require("../validators/auth");
+const {
+  requireSignin,
+  localVariable,
+  upload,
+} = require("../common-middleware");
 const router = express.Router();
-const shortid = require("shortid");
-const path = require("path");
 
 router.post("/signup", validateSignupRequest, isRequestValidated, signup);
 
 router.post("/signin", validateSigninRequest, isRequestValidated, signin);
 
-router.post('/updateprofile', requireSignin, upload.single("profilePicture"), updateProfile);
+router.post(
+  "/updateprofile",
+  requireSignin,
+  upload.single("profilePicture"),
+  updateProfile
+);
 
-router.post('/forgotpassword', forgotPassword);
+router.post("/forgotpassword", forgotPassword);
 
-router.post('/resetpassword', resetPassword);
-router.post('/requestpasswordreset', requestPasswordReset);
+router.post("/resetpassword", resetPassword);
 
-router.post('/requestVerifyEmail',localVariable, requestVerifyEmail);
-router.post('/verifyEmailViaOtp', verifyEmailViaOtp);
+router.post("/requestpasswordreset", requestPasswordReset);
 
+router.post("/requestVerifyEmail", localVariable, requestVerifyEmail);
 
-router.get('/generateOTP',localVariable, generateOTP);
+router.post("/verifyEmailViaOtp", verifyEmailViaOtp);
 
-router.post('/verifyOTP',localVariable, verifyOTP);
+router.get("/generateOTP", localVariable, generateOTP);
 
-router.post('/updatePassword',requireSignin, updatePassword);
+router.post("/verifyOTP", localVariable, verifyOTP);
 
+router.post("/updatePassword", requireSignin, updatePassword);
 
+router.post("/signout", signout);
 
-router.post('/signout', signout);
-
-router.get('/user', requireSignin,getUserData);
-
-
+router.get("/user", requireSignin, getUserData);
 
 module.exports = router;
-
